35aa0515d904f94d4f945391834c6e265f23ad2b
Sumit Sinha <sumitsinha1989@gmail.com>
2016-06-30 17:58:54 +0530

    corrected errors of porting

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ var http = require('http').Server(app);
 var _globals = require('./_globals.js');
 
 app.get('/', function (req, res) {
-    this.body = "<a href='https://github.com/login/oauth/authorize?client_id=" + _globals.client_id + "&scope=repo&redirect_uri=" + _globals.webhook_callback_url + "/chooserepo'>link</a>";
+   res.send("<a href='https://github.com/login/oauth/authorize?client_id=" + _globals.client_id + "&scope=repo&redirect_uri=" + _globals.webhook_callback_url + "/chooserepo'>link</a>");
 });
 
 app.get('/chooserepo', function (req, res) {
     var request = require('request');
     request.post(
         'https://github.com/login/oauth/access_token',
-        { json: { code: this.request.query.code, client_id:_globals.client_id , client_secret:_globals.client_secret} },
+        { json: { code: req.query.code, client_id:_globals.client_id , client_secret:_globals.client_secret} },
         function (error, response, body) {
             console.log(response);
             if (!error && response.statusCode == 200) {
                 console.log(body)
                 if(body.scope == 'repo') {
                     var token = body.access_token;
-                    var token = "111";
                     //console.log(token);
                     getReposByToken(token, function(repolist){
                         //console.log(JSON.stringify(repolist));
@@ -58,12 +57,12 @@ function getReposByToken(token, callback){
         token: token
     });
     
-    github.repos.getAll({}, function (err, res){
+    github.repos.getAll({}, function (err, resl){
         //console.log(JSON.stringify(res));
         var repolist={};
-        res.forEach(function (v,i) {
+        resl.forEach(function (v,i) {
           repolist[i]=v.name;
-          if(i==res.length-1) {
+          if(i==resl.length-1) {
               callback(repolist);
           }
         });
